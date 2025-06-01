108a16a27685bc2b7c855eff909edc145c598f12
wonbi92 <bin9239@gmail.com>
2025-06-01 14:37:08 +0900

    feat: 폴더 정리 및 Kakao App Key 연결

--- Grayzone/Grayzone/App/GrayzoneApp.swift
+++ Grayzone/Grayzone/App/GrayzoneApp.swift
@@ -6,12 +6,36 @@
 //
 
 import SwiftUI
+import ComposableArchitecture
+import KakaoMapsSDK
 
 @main
 struct GrayzoneApp: App {
+    
+    init() {
+        guard
+            let kakaoAppKey = Bundle.main.object(forInfoDictionaryKey: "KAKAO_APP_KEY") as? String
+        else {
+          fatalError("Info.plist에서 Key 정보를 읽어오지 못했습니다.")
+        }
+        
+        SDKInitializer.InitSDK(appKey: kakaoAppKey)
+    }
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            NavigationStack {
+                CompanyDetailView(
+                    store: Store(
+                        initialState: CompanyDetailFeature.State(
+                            companyID: 1,
+                            reviews: Shared(value: [])
+                        )
+                    ) {
+                        CompanyDetailFeature()
+                    }
+                )
+            }
         }
     }
 }
